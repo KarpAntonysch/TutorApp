22df78fa60474e9073ce41d46b4b4c0fcf652a86
KarpAnt <antonkarp95@mail.ru>
2022-06-06 16:49:10 +0300

    Made the basic fragments

---
+++ app/src/main/java/com/example/tutor/StudentJournalFragment.kt
@@ -0,0 +1,15 @@
+package com.example.tutor
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class StudentJournalFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
