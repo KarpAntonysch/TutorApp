406d7e9988b5e5b8b4e4332b5f26c7c999ee5885
KarpAnt <antonkarp95@mail.ru>
2022-06-06 19:12:14 +0300

    Added fragments to navigation graph and initialized navigation host in MA

--- app/src/main/java/com/example/tutor/MainActivity.kt
+++ app/src/main/java/com/example/tutor/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.tutor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 import com.example.tutor.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +12,9 @@ lateinit var binding: ActivityMainBinding
         super.onCreate(savedInstanceState)
         binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navigationView=binding.bottomMenu
+        val navigationHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
     }
 }
