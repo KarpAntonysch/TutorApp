3677bfec090dc14a4ca0222f2f63a722b6ae4843
KarpAnt <antonkarp95@mail.ru>
2022-06-08 17:47:15 +0300

    Setup navigation between fragments using NavigationUI

--- app/src/main/java/com/example/tutor/MainActivity.kt
+++ app/src/main/java/com/example/tutor/MainActivity.kt
@@ -3,7 +3,11 @@ package com.example.tutor
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.tutor.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 lateinit var binding: ActivityMainBinding
@@ -12,9 +16,23 @@ lateinit var binding: ActivityMainBinding
         super.onCreate(savedInstanceState)
         binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        val navigationView=binding.bottomMenu
-        val navigationHostFragment =
-            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+            // использую NavigationUI with Bottom navigation
+        val btmNavigationView=binding.bottomMenu
+            //инициализация хоста, позволяющая искать нужный нам фрагмент
+        val navHostFragment =
+           supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+            // инициализируем navController
+        val navController=navHostFragment.navController
+            // задаем конфигурацию меню
+        val appBarConfiguration= AppBarConfiguration(setOf(
+            R.id.mainFragment,
+            R.id.statisticFragment,
+            R.id.studentJournalFragment
+        ))
+        // связываем конфигурацию меню   с navController
+        setupActionBarWithNavController(navController,appBarConfiguration)
+        // связываем bottom navigation view  с navController
+        btmNavigationView.setupWithNavController(navController)
     }
+
 }
