e3f5c6bbb27313fc3308f2587e708590ab80d33f
KarpAnt <antonkarp95@mail.ru>
2022-06-09 12:59:05 +0300

    Added model for added student. + made bd package with entities

---
+++ app/src/main/java/com/example/tutor/bd/StudentEntiy.kt
@@ -0,0 +1,23 @@
+package com.example.tutor.bd
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.io.Serializable
+
+
+@Entity(tableName = "studentTable")
+data class StudentEntity(
+    @ColumnInfo(name="firstName")
+    val firstName: String,
+    @ColumnInfo(name="secondName")
+    val secondName: String,
+    @ColumnInfo(name="price")
+    val price: Int,
+    @ColumnInfo(name="schoolClass")
+val schoolClass: Int
+): Serializable {
+    @PrimaryKey(autoGenerate = true)
+    var id:Int=0
+
+}
