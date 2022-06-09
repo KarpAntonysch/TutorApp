eba72579dfff2ecb2005ea5ab23225f508781a84
KarpAnt <antonkarp95@mail.ru>
2022-06-09 10:16:33 +0300

    Added navigation between StudentJournalFragment.kt and AddStudentToJournalFragment.kt

--- app/src/main/java/com/example/tutor/journal/StudentJournalFragment.kt
+++ app/src/main/java/com/example/tutor/journal/StudentJournalFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.tutor.R
 import com.example.tutor.databinding.FragmentStudentJournalBinding
 
 class StudentJournalFragment : Fragment() {
@@ -17,6 +19,14 @@ class StudentJournalFragment : Fragment() {
     ): View? {
         binding = FragmentStudentJournalBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.addStudent.setOnClickListener{
+            findNavController().navigate(R.id.action_studentJournalFragment_to_addStudentToJournal)
+        }
+
     }
 
 }
