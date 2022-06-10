792af9dc539291577e7c301e62f10595e652e316
KarpAnt <antonkarp95@mail.ru>
2022-06-10 11:46:54 +0300

    Added a method to get a list of student to StudentDAO.kt and added a StudentRepository.kt

--- app/src/main/java/com/example/tutor/bd/dao/StudentDAO.kt
+++ app/src/main/java/com/example/tutor/bd/dao/StudentDAO.kt
@@ -1,13 +1,18 @@
 package com.example.tutor.bd.dao
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import com.example.tutor.bd.StudentEntity
 
 @Dao
 interface StudentDAO {
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun insertStudent(studentEntity:StudentEntity)
+    suspend fun insertStudent(studentEntity: StudentEntity)
+    // добавляю функцию для получения списка всех студентов
+    @Query("SELECT * FROM studentTable")
+    fun getAllStudents(): LiveData<List<StudentEntity>>
 }
---
+++ app/src/main/java/com/example/tutor/repository/StudentRepository.kt
@@ -0,0 +1,8 @@
+package com.example.tutor.repository
+
+import com.example.tutor.bd.dao.StudentDAO
+
+class StudentRepository(private val studentDAO: StudentDAO ) {
+
+
+}
