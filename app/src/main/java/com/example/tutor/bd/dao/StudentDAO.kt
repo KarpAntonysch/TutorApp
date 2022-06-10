a47730a0cef2d0dcbf4bc52fc10eb943cbf3fe2e
KarpAnt <antonkarp95@mail.ru>
2022-06-10 13:22:34 +0300

    Added implementations of coroutines to gradle, changed method for get student list from DAO. Changed StudentRepository.kt

--- app/src/main/java/com/example/tutor/bd/dao/StudentDAO.kt
+++ app/src/main/java/com/example/tutor/bd/dao/StudentDAO.kt
@@ -1,11 +1,12 @@
 package com.example.tutor.bd.dao
 
-import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.tutor.bd.StudentEntity
+import kotlinx.coroutines.flow.Flow
+
 
 @Dao
 interface StudentDAO {
@@ -14,5 +15,5 @@ interface StudentDAO {
     suspend fun insertStudent(studentEntity: StudentEntity)
     // добавляю функцию для получения списка всех студентов
     @Query("SELECT * FROM studentTable")
-    fun getAllStudents(): LiveData<List<StudentEntity>>
+    fun getAllStudents(): Flow<List<StudentEntity>>
 }
--- app/src/main/java/com/example/tutor/repository/StudentRepository.kt
+++ app/src/main/java/com/example/tutor/repository/StudentRepository.kt
@@ -1,8 +1,20 @@
 package com.example.tutor.repository
 
+import androidx.annotation.WorkerThread
+import com.example.tutor.bd.StudentEntity
 import com.example.tutor.bd.dao.StudentDAO
+import kotlinx.coroutines.flow.Flow
 
-class StudentRepository(private val studentDAO: StudentDAO ) {
 
+class StudentRepository(private val studentDAO: StudentDAO) {
+    // Flow может возвращать несколько объектов по очереди или сразу, suspend же возвращает один
+    // объект и завершается
+    val allStudents: Flow<List<StudentEntity>> = studentDAO.getAllStudents()
 
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun insert(studentEntity: StudentEntity) {
+        studentDAO.insertStudent(studentEntity)
+
+    }
 }
