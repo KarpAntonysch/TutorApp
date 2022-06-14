5cae5f28637ecb2a59c39948536b447176243cc2
KarpAnt <antonkarp95@mail.ru>
2022-06-14 16:23:57 +0300

    Added ListAdapter for recyclerView

--- app/src/main/java/com/example/tutor/adapters/StudentJournalAdapter.kt
+++ app/src/main/java/com/example/tutor/adapters/StudentJournalAdapter.kt
@@ -1,21 +1,26 @@
 package com.example.tutor.adapters
 
-import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-
 import com.example.tutor.R
 import com.example.tutor.bd.StudentEntity
 import com.example.tutor.databinding.StudentJournalItemBinding
 
-class StudentJournalAdapter : RecyclerView.Adapter<StudentJournalAdapter.StudentViewHolder>() {
-
-    var journalList= emptyList<StudentEntity>()
+class StudentJournalAdapter :
+    ListAdapter<StudentEntity, StudentJournalAdapter.StudentViewHolder>(StudentComparator()) {
 
     class StudentViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val binding = StudentJournalItemBinding.bind(view)
+        fun bind(studentEntity: StudentEntity) {
+            binding.tvID.text =studentEntity.id++.toString()
+            binding.tvName.text = studentEntity.firstName
+            binding.tvSecondName.text = studentEntity.secondName
+            binding.tvSchoolClass.text = studentEntity.schoolClass.toString()
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StudentViewHolder {
@@ -25,19 +30,17 @@ class StudentJournalAdapter : RecyclerView.Adapter<StudentJournalAdapter.Student
     }
 
     override fun onBindViewHolder(holder: StudentViewHolder, position: Int) {
-        holder.binding.tvID.text=journalList[position].id++.toString()
-        holder.binding.tvName.text=journalList[position].firstName
-        holder.binding.tvSecondName.text=journalList[position].secondName
-        holder.binding.tvSchoolClass.text=journalList[position].schoolClass.toString()
+        val currentStudent = getItem(position)
+        holder.bind(currentStudent)
     }
 
-    override fun getItemCount(): Int {
-        return journalList.size
-    }
-    @SuppressLint("NotifyDataSetChanged")
-    fun setList(list: List<StudentEntity>){
-        journalList=list
-        notifyDataSetChanged()
-    }
+    class StudentComparator : DiffUtil.ItemCallback<StudentEntity>() {
+        override fun areItemsTheSame(oldItem: StudentEntity, newItem: StudentEntity): Boolean {
+            return oldItem == newItem
+        }
 
+        override fun areContentsTheSame(oldItem: StudentEntity, newItem: StudentEntity): Boolean {
+            return oldItem == newItem
+        }
+    }
 }
