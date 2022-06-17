81437f5d4ed06040fe23f3ed84bbe4f681e8f552
KarpAnt <antonkarp95@mail.ru>
2022-06-17 18:01:52 +0300

    Some changed FragmentManagers for transmissions of DialogFragment's result

--- app/src/main/java/com/example/tutor/journal/studentJournal/JourmalDialogFragment.kt
+++ app/src/main/java/com/example/tutor/journal/studentJournal/JourmalDialogFragment.kt
@@ -4,10 +4,8 @@ import android.app.AlertDialog
 import android.app.Dialog
 import android.content.DialogInterface
 import android.os.Bundle
-import android.util.Log
 import androidx.core.os.bundleOf
 import androidx.fragment.app.DialogFragment
-import com.example.tutor.R
 
 class JourmalDialogFragment : DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
@@ -15,9 +13,8 @@ class JourmalDialogFragment : DialogFragment() {
             parentFragmentManager.setFragmentResult(REQUEST_KEY, bundleOf(KEY_RESPONSE to which))
         }
         return AlertDialog.Builder(requireContext())
-            .setCancelable(true)
-             .setTitle("Вы действительно хотите удалить ?")
-            //.setMessage("Вы действительно хотите удалить ?")
+            .setCancelable(false)
+            .setTitle("Вы действительно хотите удалить ?")
             .setPositiveButton("да", listener)
             .setNegativeButton("нет", listener)
             .create()
@@ -34,8 +31,11 @@ class JourmalDialogFragment : DialogFragment() {
     }
 
     companion object {
-        @JvmStatic val TAG = JourmalDialogFragment::class.java.simpleName
-        @JvmStatic val REQUEST_KEY = "$TAG:defaultRequestKey"
-        @JvmStatic val KEY_RESPONSE = "RESPONSE"
+        @JvmStatic
+        val TAG = JourmalDialogFragment::class.java.simpleName
+        @JvmStatic
+        val REQUEST_KEY = "$TAG:defaultRequestKey"
+        @JvmStatic
+        val KEY_RESPONSE = "RESPONSE"
     }
 }
--- app/src/main/java/com/example/tutor/journal/studentJournal/StudentJournalFragment.kt
+++ app/src/main/java/com/example/tutor/journal/studentJournal/StudentJournalFragment.kt
@@ -1,15 +1,11 @@
 package com.example.tutor.journal.studentJournal
 
-import android.app.AlertDialog
-import android.app.Dialog
 import android.content.DialogInterface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentResultListener
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -22,14 +18,14 @@ import com.example.tutor.journal.StudentJournalViewModel
 import com.example.tutor.journal.StudentJournalViewModelFactory
 
 
-
-class StudentJournalFragment : Fragment(),StudentJournalAdapter.Listener {
+class StudentJournalFragment : Fragment(), StudentJournalAdapter.Listener {
     lateinit var binding: FragmentStudentJournalBinding
     lateinit var recyclerView: RecyclerView
-    private var adapter=StudentJournalAdapter(this)
+    private var adapter = StudentJournalAdapter(this)
     private val studentJournalViewModel: StudentJournalViewModel by viewModels {
         StudentJournalViewModelFactory((requireActivity().application as StudentApplication).repository)
     }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -38,6 +34,7 @@ class StudentJournalFragment : Fragment(),StudentJournalAdapter.Listener {
         binding = FragmentStudentJournalBinding.inflate(inflater, container, false)
         return binding.root
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.btnAddStudent.setOnClickListener {
@@ -47,37 +44,42 @@ class StudentJournalFragment : Fragment(),StudentJournalAdapter.Listener {
 
 
     }
-    private fun realizationOfRV(){
-        recyclerView=binding.allStudentsRV
-        recyclerView.adapter=adapter
+
+    private fun realizationOfRV() {
+        recyclerView = binding.allStudentsRV
+        recyclerView.adapter = adapter
         studentJournalViewModel.allStudents.observe(viewLifecycleOwner)
-            {studentList ->
-                studentList.let { adapter.submitList(it) }
-            }
+        { studentList ->
+            studentList.let { adapter.submitList(it) }
+        }
     }
 
     // функция из интерфеса для открытия диалогового окна
     override fun openDialogFragment() {
         showDialogFragment()
     }
+
     // функция из интерфейса для подстверждения удаления
     override fun onClickToDeleteStudent(studentEntity: StudentEntity) {
         setupDialogFragmentListener(studentEntity)
     }
 
     // Функция вызова диалогового окна из JournalDialogFragment
-    fun showDialogFragment(){
+    fun showDialogFragment() {
         val dialogFragment = JourmalDialogFragment()
-        dialogFragment.show(activity!!.supportFragmentManager,JourmalDialogFragment.TAG)
+        dialogFragment.show(childFragmentManager, JourmalDialogFragment.TAG)
     }
+
     // Функция инициализации кнопок в диалоговом окне из JournalDialogFragment
-    fun setupDialogFragmentListener(studentEntity: StudentEntity){
-        activity!!.supportFragmentManager.setFragmentResultListener(JourmalDialogFragment.REQUEST_KEY,this,
-        FragmentResultListener{_,result ->
-            val which = result.getInt(JourmalDialogFragment.KEY_RESPONSE)
-            when(which){
-                DialogInterface.BUTTON_POSITIVE -> studentJournalViewModel.deleteStudent(studentEntity)
-            }
-        })
+    fun setupDialogFragmentListener(studentEntity: StudentEntity) {
+        childFragmentManager.setFragmentResultListener(JourmalDialogFragment.REQUEST_KEY,
+            this,
+            FragmentResultListener { _, result ->
+                val which = result.getInt(JourmalDialogFragment.KEY_RESPONSE)
+                when (which) {
+                    DialogInterface.BUTTON_POSITIVE -> studentJournalViewModel.deleteStudent(
+                        studentEntity)
+                }
+            })
     }
 }
