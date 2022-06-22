742f4ebf2a3f3d16baf52c01365e1869359bf751
KarpAnt <antonkarp95@mail.ru>
2022-06-22 15:39:56 +0300

    Made Room TutorDataBase  Migrations for new entity(ScheduleEntity)

--- app/src/main/java/com/example/tutor/bd/TutorDataBase.kt
+++ app/src/main/java/com/example/tutor/bd/TutorDataBase.kt
@@ -4,11 +4,14 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.example.tutor.bd.dao.ScheduleDAO
 import com.example.tutor.bd.dao.StudentDAO
+import com.example.tutor.bd.entities.ScheduleEntity
 import com.example.tutor.bd.entities.StudentEntity
 
-@Database(entities = [StudentEntity::class], version = 1)
+@Database(entities = [StudentEntity::class, ScheduleEntity::class], version = 4)
 abstract class TutorDataBase : RoomDatabase() {
     // для каждой DAO определяем метод для свзязи с этой DAO
     abstract fun getStudentDAO(): StudentDAO
@@ -21,11 +24,23 @@ abstract class TutorDataBase : RoomDatabase() {
         @Synchronized//защита от одновременного выполнения несколькими потоками
         fun getDataBase(context: Context): TutorDataBase { //  инициализируем БД
             return if (database == null) {
-                database = Room.databaseBuilder(context, TutorDataBase::class.java, "bd").build()
+                database = Room.databaseBuilder(context, TutorDataBase::class.java, "bd")
+                    .addMigrations(MIGRATION_1_4)
+                    .allowMainThreadQueries()
+                    .build()
                 database as TutorDataBase
             } else {
                 database as TutorDataBase
             }
         }
+    }
+}
+
+val MIGRATION_1_4 = object : Migration(1, 4) {
+    override fun migrate(database: SupportSQLiteDatabase) {
+        database.execSQL("CREATE TABLE scheduleTable" +
+                " (dateWithTime INTEGER NOT NULL," +
+                " studentId INTEGER NOT NULL," +
+                " id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL)")
     }
 }
