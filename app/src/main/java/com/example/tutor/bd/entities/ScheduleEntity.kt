dad17f699a03a6f353aabc1c63ff55f75f37bbdc
KarpAnt <antonkarp95@mail.ru>
2022-06-22 12:18:49 +0300

    Added ScheduleEntity.kt

---
+++ app/src/main/java/com/example/tutor/bd/entities/ScheduleEntity.kt
@@ -0,0 +1,19 @@
+package com.example.tutor.bd.entities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.io.Serializable
+
+
+@Entity(tableName = "scheduleTable")
+data class ScheduleEntity(
+    @ColumnInfo(name ="dateWithTime")
+    val dateWithTime:Long,
+    // указываем id студента,для дальнейшего связывания таблиц
+    @ColumnInfo(name ="studentId")
+    val studentId:Int
+) : Serializable{
+    @PrimaryKey(autoGenerate = true)
+    var id:Int=0
+}
