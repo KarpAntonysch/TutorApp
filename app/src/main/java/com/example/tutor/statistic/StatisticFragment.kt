16cf666e25c0b71bc78497b8bb13d36738eb2858
KarpAnt <antonkarp95@mail.ru>
2022-07-25 13:13:20 +0300

    Added the test char and current date to the StatisticFragment.kt

--- app/src/main/java/com/example/tutor/statistic/StatisticFragment.kt
+++ app/src/main/java/com/example/tutor/statistic/StatisticFragment.kt
@@ -5,11 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.tutor.R
+import com.example.tutor.convertLongToTime
 import com.example.tutor.databinding.FragmentMainBinding
 import com.example.tutor.databinding.FragmentStatisticBinding
+import com.github.aachartmodel.aainfographics.aachartcreator.*
+import java.util.*
 
 class StatisticFragment : Fragment() {
     lateinit var binding: FragmentStatisticBinding
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,4 +25,41 @@ class StatisticFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val aaChartView = binding.aaChartView
+        aaChartView.aa_drawChartWithChartModel(weekChart())
+        val currentDate =getCurrentTime()
+        binding.textView.text = currentDate.convertLongToTime("dd.MM.yyyy")
+    }
+
+    fun getCurrentTime(): Long {
+        val cal: Calendar = Calendar.getInstance()
+        return cal.timeInMillis
+    }
+    // функция для отображения недельного графика из библиотеки AAChartModel
+    fun weekChart():AAChartModel{
+        val aaChartModel : AAChartModel = AAChartModel()
+            .chartType(AAChartType.Column)
+            //.title("Деньги")
+            //.subtitle("неделя")
+            .backgroundColor("#FFFFFFFF")
+            .yAxisTitle("Рубль")
+            .dataLabelsEnabled(true)
+            .categories(arrayOf(
+                "qq","ww","ee","rr","xx","aa","ss"
+            ))
+            .series(arrayOf(
+                AASeriesElement()
+                    .name("Количесвто занятий")
+                    .data(arrayOf(2000, 432, 3000, 454, 590, 530, 510))
+                    .step("right")//
+                , AASeriesElement()
+                    .name("Заработано")
+                    .data(arrayOf(220, 282, 201, 234, 290, 430, 410))
+                    .step("center")//设置折线样式为直方折线,折线连接点位置居中
+            )
+            )
+        return aaChartModel
+    }
 }
