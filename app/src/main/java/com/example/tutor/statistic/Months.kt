ec8c17414c2a57d39f84a75002e860f0b9f5252f
KarpAnt <antonkarp95@mail.ru>
2022-08-03 18:57:45 +0300

    Added the Month enam class for previous commit

---
+++ app/src/main/java/com/example/tutor/statistic/Months.kt
@@ -0,0 +1,16 @@
+package com.example.tutor.statistic
+
+enum class Months(val rusName:String, val number: Int) {
+    JANUARY("ЯНВАРЬ",1),
+    FEBRUARY("ФЕВРАЛЬ",2),
+    MARCH("МАРТ",3),
+    APRIL("АПРЕЛЬ",4),
+    MAY("МАЙ",5),
+    JUNE("ИЮНЬ",6),
+    JULY("ИЮЛЬ",7),
+    AUGUST("АВГУСТ",8),
+    SEPTEMBER("СЕНТЯБРЬ",9),
+    OCTOBER("ОКТЯБРЬ",10),
+    NOVEMBER("НОЯБРЬ",11),
+    DECEMBER("ДЕКАБРЬ",12);
+}
