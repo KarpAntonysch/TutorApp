51280de50a20f484105a9df4f17d9ac1ca4ef5a1
KarpAnt <antonkarp95@mail.ru>
2022-08-07 13:22:11 +0300

    Reverted the old version of JournalPagerFragment

--- app/src/main/java/com/example/tutor/journal/studentJournal/pager/JornalPagerFragment.kt
+++ app/src/main/java/com/example/tutor/journal/studentJournal/pager/JornalPagerFragment.kt
@@ -1,29 +1,16 @@
 package com.example.tutor.journal.studentJournal.pager
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.MutableLiveData
-import com.example.tutor.R
 import com.example.tutor.adapters.JournalVPAdapter
-import com.example.tutor.bd.entities.StudentEntity
 import com.example.tutor.databinding.FragmentJornalPagerBinding
-import com.example.tutor.journal.StudentJournalViewModel
-import com.example.tutor.journal.StudentJournalViewModelFactory
-import com.example.tutor.journal.studentJournal.DBapplication
-import com.example.tutor.journal.studentJournal.pager.activeStudents.JournalActionModeCallback
 import com.google.android.material.tabs.TabLayoutMediator
 
-class JornalPagerFragment : Fragment(), JournalActionModeCallback.ActionModeListener {
+class JornalPagerFragment : Fragment() {
     lateinit var binding: FragmentJornalPagerBinding
-    lateinit var actionMode: JournalActionModeCallback
-    private val studentJournalViewModel: StudentJournalViewModel by activityViewModels {
-        StudentJournalViewModelFactory((requireActivity().application as DBapplication).studentRepository)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -36,18 +23,6 @@ class JornalPagerFragment : Fragment(), JournalActionModeCallback.ActionModeList
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setPagerAndTab()
-        Log.v("aaa",
-            "1${studentJournalViewModel.actionModeVM.value},${studentJournalViewModel.student.value} onViewCreated JornalPagerFragment")
-        studentJournalViewModel.actionModeVM.observe(viewLifecycleOwner) { acshn ->
-            if (acshn == true) {
-                Log.v("aaa",
-                    "1${studentJournalViewModel.actionModeVM.value},1${studentJournalViewModel.student.value}")
-                actionMode =
-                    JournalActionModeCallback(studentJournalViewModel.student)
-                actionMode.startActionMode(view!!, R.menu.journal_app_menu, this)
-            }
-
-        }
     }
 
     fun setPagerAndTab() {
@@ -61,9 +36,5 @@ class JornalPagerFragment : Fragment(), JournalActionModeCallback.ActionModeList
                 else -> tab.setText("Выпускники")
             }
         }.attach()
-    }
-
-    override fun clickToMenuDelete(studentEntity: MutableLiveData<StudentEntity>?) {
-        TODO("Not yet implemented")
     }
 }
